98622d07785db1c509f3286b0d79375b6bdf7a6f
RNalban <73193741+RNalban@users.noreply.github.com>
2021-02-20 11:42:57 +0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,13 @@
 <?php
-$username = "root";
-$password = "";
-$server = "localhost";
-$db="bank";
+
+//$username = "root";
+//$password = "";
+//$server = "localhost";
+//$db="bank";
+$username = "b602f590bbebec";
+$password = "d9aafa23";
+$server = "us-cdbr-east-03.cleardb.com";
+$db="heroku_75f8b5c954546c4";
 $con=mysqli_connect($server,$username,$password,$db);
 if(!$con){
     die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
